20438a284b4c8fd85959c21556d53f0bb7c717c8
jerickvasquez <jorgeerick446@gmail.com>
2022-04-21 10:34:05 -0500

    Ejemplo del patron Singleton

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using patronSingleton;
+
+
+Console.WriteLine(Singleton.Instance.mensaje);
+Singleton.Instance.mensaje = "Hola Marte";
+Console.WriteLine(Singleton.Instance.mensaje);
+
---
+++ Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patronSingleton
+{
+        public class Singleton
+        {
+            private static Singleton _instance = null;
+            public string mensaje = "";
+            protected Singleton()
+            {
+            mensaje = "Hola Mundo";
+            }
+
+            public static Singleton Instance
+            {
+                get
+                {
+                    if (_instance == null)
+                        _instance = new Singleton();
+
+                    return _instance;
+                }
+            }
+
+        }
+    
+}
